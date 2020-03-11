62b433339b1c3642c93cfabc4be8b92718064b0b
VadimAlexandru <v700.lg@gmail.com>
2020-03-11 12:26:06 +0200

    add lab 1

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,27 @@
 
+import kotlin.math.cos
+import kotlin.math.exp
+import kotlin.math.sin
 
 
+fun main() {
 
-fun main(args: Array<String>) {
-    println("labs")
+    val lab1 = Lab1(5, 1,1,0.1)
+
+    lab1.phi = {
+        10 * cos((3.14 / 2) * it)
+    }
+    lab1.mu1 = {
+        10 * exp(it)
+    }
+    lab1.mu2 = {
+        sin(it)
+    }
+    lab1.func = { x, _ ->
+        2 * sin(2 * x) * sin(x)
+    }
+
+    lab1.getGrid(0.5)
+
 }
+
